d1927bf23e9df1f623b14d09cc7765214cdfca05
Kenny Liang <kenneth.liang12@gmail.com>
2014-08-31 01:01:09 -0400

    still working on count_analytics unittest; including googleplus into it - test objects for twitter and googleplus and placeholders

--- analytics/tests/count_analytics_test.py
+++ analytics/tests/count_analytics_test.py
@@ -1,6 +1,6 @@
 import unittest
 import datetime
-from shareablee_socialMediaMetrics.analytics import count_analytics
+from shareablee_socialMediaMetrics.analytics.count_analytics import UserCounts
 from shareablee_socialMediaMetrics.dataextractors.mediadata_extractor import getMediaData
 
 class Test_count_analytics(unittest.TestCase):
@@ -8,27 +8,33 @@ class Test_count_analytics(unittest.TestCase):
         self.date_Jan01 = datetime.date(2014,1,1)
         self.date_Aug05 = datetime.date(2014,8,5)
         self.date_Sep05 = datetime.date(2014,9,5)
-        self.class_UserCounts = count_analytics.UserCounts
+
         self.twitterData = getMediaData('twitter', "286200117457846272",
                                         self.date_Jan01, self.date_Aug05)
+        self.twitterData['created_at']=datetime.date(2014,1,1)
+        # # what's the key for twitter's comment?
+        self.twitterobj_UserCounts = UserCounts(self.twitterData,
+                                               'favorites_count', '',
+                                               'retweet_count', 'created_at')
+
         self.googleplusData = getMediaData('googleplus', "100470681032489535736",
                                         self.date_Jan01, self.date_Aug05)
+        self.googleplusData['updated']=datetime.date(2014,7,15)
+        self.googleplusobj_UserCounts = UserCounts(self.googleplusData,
+                                                   'plusones_count', 'comments_count',
+                                                   'reshares_count', 'updated')
 
     def test_getAllData(self):
-        # # what's the key for twitter's comment?
-        self.class_UserCounts(self.twitterData, 'favorites_count', '', 'retweet_count', 'created_at')
+        return
 
     def test_getAllFavorites(self):
-        # # what's the key for twitter's comment?
-        self.class_UserCounts(self.twitterData, 'favorites_count', '', 'retweet_count', 'created_at')
+        return
 
     def test_getAllComments(self):
-        # # what's the key for twitter's comment?
-        self.class_UserCounts(self.twitterData, 'favorites_count', '', 'retweet_count', 'created_at')
+        return
 
     def test_getAllShares(self):
-        # # what's the key for twitter's comment?
-        self.class_UserCounts(self.twitterData, 'favorites_count', '', 'retweet_count', 'created_at')
+        return
 
 
 if __name__ == '__main__':
